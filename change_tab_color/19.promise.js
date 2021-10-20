00763446cdae49a1ee6b467a7905de577f6d840a
bai <1030691053@qq.com>
2021-10-20 21:03:54 +0800

    10-20-study

--- change_tab_color/19.promise.js
+++
@@ -1,6 +0,0 @@
-import thenFS from 'then-fs'
-
-// 不能保证
-thenFS.readFile('./file/1.txt','utf-8').then(r1=>{console.log(r1);},err=>{console.log(err.message);})
-thenFS.readFile('./file/2.txt','utf-8').then(r2=>{console.log(r2);},err=>{console.log(err.message);})
-thenFS.readFile('./file/3.txt','utf-8').then(r3=>{console.log(r3);},err=>{console.log(err.message);})
